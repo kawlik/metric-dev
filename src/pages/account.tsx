61a6ad4269926394c2717db4bbc7e1c8cfe37d18
kawlik <kpawlik1103@gmail.com>
2022-10-09 00:01:23 +0200

    Code update

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,8 +1,12 @@
-import { AppViewStack } from '../components/@';
+import { AppPageEmpty, AppViewStack } from '../components/@';
 
 export default function (props: {}) {
 	// component logic
 
 	// component layout
-	return <AppViewStack flex={1} />;
+	return (
+		<AppViewStack flex={1}>
+			<AppPageEmpty />
+		</AppViewStack>
+	);
 }
