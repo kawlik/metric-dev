51cf20d1e7a9d2751746606f5fd8a5d6c58d32c9
kawlik <kpawlik1103@gmail.com>
2022-10-23 19:23:40 +0200

    Code update

--- src/components/app-page-splash.tsx
+++ src/components/app-page-splash.tsx
@@ -1,19 +1,8 @@
-import { Avatar, LinearProgress, Stack, Typography } from '@mui/material';
-
-// assets
-import Gif from '../assets/checklist.gif';
+import { AppViewLoading } from './@';
 
 export default function (props: {}) {
 	// component logic
 
 	// component layout
-	return (
-		<Stack alignItems={'center'} flex={1} gap={1} justifyContent={'center'}>
-			<Avatar src={Gif} sx={{ height: 128, width: 128 }} variant={'rounded'} />
-			<Typography textAlign={'center'} variant={'h6'}>
-				<LinearProgress />
-				I'm already warming up pens!
-			</Typography>
-		</Stack>
-	);
+	return <AppViewLoading isLoading={true} />;
 }
